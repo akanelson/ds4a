aae6b97b12b46250cbdca2f7c84a285c837df95c
kecol <ezequiel.bidart@gmail.com>
2020-03-19 18:16:46 -0300

    First Commit

---
+++ initial_exploratory_analysis/practicum_utils.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import random
+
+# utils functions to work with first data availale
+dir_data = '../data/supply/'
+
+def get_loggi_files():
+	files = [
+	    'availability_dist1_ano.csv',
+	    'availability_dist2_ano.csv',
+	    'itinerary_dist1_ano.csv',
+	    'itinerary_dist2_ano.csv'
+	]
+	return [dir_data + f for f in files]
+
+def read_csv_percent(filename, percent):
+    # keep the header and the 'percent' of lines
+    return pd.read_csv(
+         filename,
+         header=0, 
+         skiprows=lambda i: i > 0 and random.random() > percent/100
+    )
+
+def read_csv_every(filename, k):
+    # keep the header and the lines at multiple of k locations
+     return pd.read_csv(filename, header=0, skiprows=lambda i: i % k != 0)
+
+
+# Sequential Access
+#chunksize = 10 ** 5
+#for chunk in pd.read_csv(files[0], chunksize=chunksize):
+#    process(chunk)
+#    print(chunk.info())
+#    break
+#chunk.head()
