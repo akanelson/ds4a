a30628ea9a78518dda5d288541dac0a232f2e6c2
Ariel Kanelson <ariel@mobomo.com>
2020-03-28 10:54:33 -0300

    weather json file name date formatting

--- initial_exploratory_analysis/weather.py
+++ initial_exploratory_analysis/weather.py
@@ -7,7 +7,7 @@ from practicum_config import dark_sky_api
 #
 # Get weather data from Dark Sky API Service https://darksky.net/dev
 #
-# Receives: API Key, latitude, longitude, stsart date, end date (end date not included) 
+# Receives: API Key, latitude, longitude, stsart date, end date (end date included) 
 # Returns: Dictionary with key: value where key is the date and value is the API response for that day
 #
 # Sample call:
@@ -17,12 +17,14 @@ from practicum_config import dark_sky_api
 def dark_sky_get(key, lat, lon, start_date, end_date):
     params = {'exclude': 'currently,hourly,flags'}
     # Convert dates to the format required by the API
-    start_date = dt.strptime(start_date, '%Y-%m-%d %H:%M:%S')
-    end_date = dt.strptime(end_date, '%Y-%m-%d %H:%M:%S')
+    #start_date = dt.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+    start_date = dt.strptime(start_date, '%Y-%m-%d')
+    #end_date = dt.strptime(end_date, '%Y-%m-%d %H:%M:%S')
+    end_date = dt.strptime(end_date, '%Y-%m-%d')
     # Define weather dictionary variable
     data = {}
     # For each day on the range
-    for n in range(int((end_date - start_date).days)):
+    for n in range(int((end_date - start_date).days) + 1):
         day = start_date + timedelta(n)
         day = day.strftime("%Y-%m-%dT%H:%M:%S")
         # Build URL
@@ -40,12 +42,13 @@ def dark_sky_get(key, lat, lon, start_date, end_date):
 latitude = -23.5475006
 longitude = -46.6361084
 # From date to date range
-from_date = '2019-10-01 00:00:00'
-to_date = '2020-03-13 00:00:00'
+from_date = '2019-10-01'
+to_date = '2020-03-12'
+#print(from_date[:10])
 # Get data
 data = dark_sky_get(dark_sky_api, latitude, longitude, from_date, to_date)
 print('Writing file ...')
 # Write json to file
-with open('../data/weather_daily_' + from_date + '_' + to_date +'.json', 'w') as outfile:
+with open('../data/weather_daily_' + from_date[:10] + '_' + to_date[:10] +'.json', 'w') as outfile:
     json.dump(data, outfile)
 print('Process complete!')
