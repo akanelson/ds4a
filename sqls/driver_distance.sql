6430b32cb84918a9f2c17a06afeab00c142ff793
Ariel Kanelson <ariel@mobomo.com>
2020-04-10 18:29:22 -0300

    Adding distance columns for assigned itineraryes pushes.

--- sqls/driver_distance.sql
+++ sqls/driver_distance.sql
@@ -14,6 +14,42 @@
  * */
 
 
+CREATE OR REPLACE FUNCTION public.calculate_distance(lat1 double precision, lon1 double precision, lat2 double precision, lon2 double precision, units character varying)
+ RETURNS double precision
+ LANGUAGE plpgsql
+AS $function$
+    DECLARE
+        dist float = 0;
+        radlat1 float;
+        radlat2 float;
+        theta float;
+        radtheta float;
+    BEGIN
+        IF lat1 = lat2 OR lon1 = lon2
+            THEN RETURN dist;
+        ELSE
+            radlat1 = pi() * lat1 / 180;
+            radlat2 = pi() * lat2 / 180;
+            theta = lon1 - lon2;
+            radtheta = pi() * theta / 180;
+            dist = sin(radlat1) * sin(radlat2) + cos(radlat1) * cos(radlat2) * cos(radtheta);
+
+            IF dist > 1 THEN dist = 1; END IF;
+
+            dist = acos(dist);
+            dist = dist * 180 / pi();
+            dist = dist * 60 * 1.1515;
+
+            IF units = 'K' THEN dist = dist * 1.609344; END IF;
+            IF units = 'N' THEN dist = dist * 0.8684; END IF;
+
+            RETURN dist;
+        END IF;
+    END;
+$function$
+;
+
+
 DROP TABLE if exists public.driver_day_distance;
 
 CREATE TABLE public.driver_day_distance (
@@ -21,12 +57,12 @@ CREATE TABLE public.driver_day_distance (
 	"date" timestamp NOT NULL,
 	day_distance float8 NULL,
 	day_total_sent int4 NULL,
+	day_distance_with_it float8 NULL,
+	day_total_sent_with_it int4 NULL,
 	CONSTRAINT driver_day_distance_pk PRIMARY KEY (driver_id, date)
 );
-CREATE INDEX driver_day_distance_driver_id_idx ON public.driver_day_distance USING btree (driver_id);
 
-
---truncate table driver_day_distance;
+--CREATE INDEX driver_day_distance_driver_id_idx ON public.driver_day_distance USING btree (driver_id);
 
 do $drivers$
 DECLARE
@@ -41,11 +77,13 @@ DECLARE
 	prev_lng float(8);
 	prev_date date;
 	diff_push FLOAT(10);
-	push_tolerance INTEGER default 300; -- seconds between pushes to be considered as consecutive;
+	push_tolerance INTEGER default 600; -- seconds between pushes to be considered as consecutive;
 	push_distance float(8);
 	total_distance float(8);
 	day_distance float(8);
+	day_distance_it float(8);
 	day_pushes integer;
+	day_pushes_it integer;
 	total_drivers integer;
 	drivers_count integer;
 
@@ -69,7 +107,9 @@ begin
 		prev_date := null;
 		total_distance := 0;
 		day_distance := 0;
-		day_pushes := 1;
+		day_pushes := 0;
+		day_distance_it := 0;
+		day_pushes_it := 0;
 		drivers_count := drivers_count + 1;
 	    
 		--raise notice '';
@@ -99,21 +139,30 @@ begin
 			end if;
 			
 			--raise notice '    # % Prev: % - Curr % - Diff % - Distance % - Day Distance % - Total Distance % - PrevPos % - CurrPos % ', day_pushes, prev_push, curr_push, diff_push, push_distance, day_distance, total_distance, cast(prev_lat as varchar)||','||cast(prev_lng as varchar), cast(curr_lat as varchar)||','||cast(curr_lng as varchar);
-
+		
 			if curr_date = prev_date then
 				day_distance := day_distance + push_distance;
 				day_pushes := day_pushes + 1;
+				
+				if push.itinerary_id is not null then
+					--raise notice ' IT ASSIGNED   # % - Day Distance % - With ITINERARY %', day_pushes_it, day_distance, day_distance_it;
+					day_distance_it := day_distance_it + push_distance;
+					day_pushes_it := day_pushes_it + 1;
+				end if;
+			
 			else
 			
 				-- Insert calculated day data into a new row
-				insert into driver_day_distance (driver_id, date, day_distance, day_total_sent )
-				values (driver.driver_id, prev_date, day_distance, day_pushes);
+				insert into driver_day_distance (driver_id, date, day_distance, day_total_sent, day_distance_with_it, day_total_sent_with_it)
+				values (driver.driver_id, prev_date, day_distance, day_pushes, day_distance_it, day_pushes_it);
 			
 				--raise notice '';
 				--raise notice ' END DAY     - Driver: % --- Date: % --- Day Distance: % --- Day pushes % -----------', driver.driver_id, prev_date, day_distance, day_pushes;
 				--raise notice '';
 				day_distance := 0;
-				day_pushes := 1;
+				day_pushes := 0;
+				day_distance_it := 0;
+				day_pushes_it := 0;				
 			end if;
 		
 			total_distance := total_distance + push_distance;
@@ -132,15 +181,31 @@ begin
 	raise notice '';
 	raise notice '';	
 	raise notice '========== END PROCESS =============';
-
-	select driver_id, 
-		round(cast(sum(day_distance) as decimal), 2) as total_distance, 
-		sum(day_total_sent) as total_sent, 
-		round(cast(avg(day_distance) as decimal), 2) as avg_day_distance, 
-		round(cast(stddev(day_distance) as decimal), 2) as std_day_distance
-	from driver_day_distance ddd 
-	group by driver_id
           
 END $drivers$
 LANGUAGE 'plpgsql'
+
+
+/*
+ * After the script above runs, run the following query
+ * to get a consolidated values per Driver.
+ * 
+
+select driver_id, 
+	round(cast(sum(day_distance) as decimal), 2) as total_distance, 
+	sum(day_total_sent) as total_sent, 
+	round(cast(avg(day_distance) as decimal), 2) as avg_day_distance, 
+	round(cast(stddev(day_distance) as decimal), 2) as std_day_distance,
+	round(cast(sum(day_distance_with_it) as decimal), 2) as total_distance_with_it, 
+	sum(day_total_sent_with_it) as total_sent_with_it, 
+	round(cast(avg(day_distance_with_it) as decimal), 2) as avg_day_distance_with_it, 
+	round(cast(stddev(day_distance_with_it) as decimal), 2) as std_day_distance_with_it
+from driver_day_distance ddd 
+group by driver_id;
+
+*/
+
+
+
+
 
