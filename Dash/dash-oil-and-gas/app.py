a9db2b1a815cb9accd1e0f817c86af8dd2be2a64
Ariel Kanelson <ariel@mobomo.com>
2020-04-24 19:12:36 -0300

    app change

--- Dash/dash-oil-and-gas/app.py
+++ Dash/dash-oil-and-gas/app.py
@@ -707,4 +707,4 @@ def make_count_figure(well_statuses, well_types, year_slider):
 
 # Main
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
