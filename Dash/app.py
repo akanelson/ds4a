37b5c72cfa23465c8e8d120ebc4966273f41149c
Ariel Kanelson <ariel@mobomo.com>
2020-04-24 20:29:18 -0300

    kecol wachiturro

--- Dash/app.py
+++ Dash/app.py
@@ -1,6 +1,26 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd 
+import plotly.express as px
+from dash.dependencies import Input, Output
+
+
+import practicum_utils as utils
+from practicum_utils import get_loggi_files, global_connect, run_query, explained_time, careful_query
+db = global_connect()
+df = careful_query("""
+SELECT DATE(created), COUNT(1) as cant,
+        SUM(CASE WHEN status = 'finished' THEN 1 ELSE 0	END) as s_finished,
+        SUM(CASE WHEN status != 'finished' THEN 1 ELSE 0 END) as s_notfinished,
+        SUM(CASE WHEN status = 'dropped' THEN 1 ELSE 0 END) as s_dropped
+FROM ITINERARIES
+GROUP BY DATE(created)
+ORDER BY DATE(created)
+""")
+#fig = px.histogram(df.s_finished)
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,22 +31,24 @@ colors = {
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
 
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    dcc.RadioItems(
+        id='type',
+        options=[
+            {'label': 'Finished', 'value': 's_finished'},
+            {'label': 'Not Finished', 'value': 's_notfinished'},
+            {'label': 'Dropped', 'value': 's_dropped'}
+        ],
+        value='s_finished'
+    ),    
 
     dcc.Graph(
-        id='example-graph-2',
+        id='loggi'
+    ),
+
+    dcc.Graph(
+        id='itineraries',
         figure={
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
@@ -41,6 +63,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             }
         }
     ),
+
 
     html.Div([
         html.Label('Dropdown'),
@@ -96,6 +119,13 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         ),
     ], style={'columnCount': 3})
 ],)
+
+@app.callback(
+    Output('loggi', 'figure'),
+    [Input(component_id='type', component_property='value')]
+)
+def cualquier_cosa_que_este_debajo(input_value):    
+    return px.scatter(df, x="s_finished", y=input_value)
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8080)
---
+++ Dash/practicum_utils.py
@@ -0,0 +1,58 @@
+import practicum_config as config
+import pandas as pd
+import random
+from sqlalchemy import create_engine, text
+
+
+# utils functions to work with first data availale
+dir_data = '../data/supply/'
+
+def get_loggi_files():
+	files = [
+	    'availability_dist1_ano.csv',
+	    'availability_dist2_ano.csv',
+	    'itinerary_dist1_ano.csv',
+	    'itinerary_dist2_ano.csv'
+	]
+	return [dir_data + f for f in files]
+
+def read_csv_percent(filename, percent):
+    # keep the header and the 'percent' of lines
+    return pd.read_csv(
+         filename,
+         header=0, 
+         skiprows=lambda i: i > 0 and random.random() > percent/100
+    )
+
+def read_csv_every(filename, k):
+    # keep the header and the lines at multiple of k locations
+     return pd.read_csv(filename, header=0, skiprows=lambda i: i % k != 0)
+
+# connect to postgre db or throw an error
+def global_connect():
+    global db
+    db = create_engine(config.postgre_conn, max_overflow=20)
+    return db
+
+def run_query(sql, df=True):
+    result = db.connect().execution_options(isolation_level="AUTOCOMMIT").execute((text(sql)))
+    if df:
+        return pd.DataFrame(result.fetchall(), columns=result.keys())
+    return result
+
+def explained_time(sql):
+    r = db.connect().execution_options(isolation_level="AUTOCOMMIT").execute((text('EXPLAIN ' + sql)))
+    row = r.fetchone()[0]
+    return float(row[row.find('cost=')+5:row.find('..')])
+
+# This procedure use EXPLAIN to check if the sql query can be resolved in less than max_seconds time
+# and then return run_query(sql) if this is True or return None if max_seconds is reached
+# TODO: make it work with seconds if possible
+def careful_query(sql, max_cost=100000):
+    estimated = explained_time(sql)
+    if estimated < max_cost:
+        return run_query(sql)
+    print('This query will cost too much: {} cost units'.format(estimated))
+    if estimated/max_cost < 5:
+        print('You could try increasing max_cost for careful_query')
+    return None
