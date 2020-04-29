e2b5202d6bbb047f0d56c82c0d02e7a553ecd6d3
kecol <ezequiel.bidart@gmail.com>
2020-04-29 00:23:05 -0300

    csv2db2 now upload availabilities datasets

--- data/csv2db2.py
+++ data/csv2db2.py
@@ -1,4 +1,5 @@
 import os, sys
+from shutil import which
 # Add directory where we have our configuration
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__ + '\\..')) + '\\initial_exploratory_analysis\\')
 print(os.path.abspath(__file__ + '..\\'))
@@ -176,3 +177,92 @@ for idx in ['driver_id', 'created_time', 'accepted_time', 'cancellation_time', '
 
     print('Creating index "{}_iidx"...'.format(idx))
     r = utils.run_query(create_simple_index_tpl.format(idx), df=False)
+
+# Agency 1: 6e7dacf2149d053183fe901e3cfd8b82
+# Agency 2: 58cfe3b975dd7cbd1ac84d555640bfd9
+
+# --------------
+# AVAILABILITIES
+# --------------
+
+# Check if table already exists
+df = utils.run_query("""select t.table_name FROM information_schema.tables as t WHERE t.table_name = 'availabilities'""")
+if len(df) == 1:
+    print('Table availabilities already exists')
+    if len(sys.argv) > 1 and sys.argv[1] == 'drop':
+        print('Forced drop...')
+        utils.run_query("""DROP TABLE availabilities""", df=False)
+    else:
+        exit()
+
+
+tpl = ["SELECT '{}' as distribution_center, * INTO availabilities FROM tmp_availabilities",
+       "INSERT INTO availabilities SELECT '{}' as distribution_center, * FROM tmp_availabilities"]
+
+for fn, agency, _tpl in [
+    ('availability_dist1_ano.csv', '6e7dacf2149d053183fe901e3cfd8b82', tpl[0]),
+    ('availability_dist2_ano.csv', '58cfe3b975dd7cbd1ac84d555640bfd9', tpl[1])]:
+
+    print('Creating temporal table for availabilies.\nLoading csv file {}.\nThis will take some time...'.format(fn))
+
+    create_table_from_csv = """
+        CREATE TEMPORARY TABLE tmp_availabilities
+        (
+            temp            INT,
+            id              VARCHAR(32) NOT NULL,
+            driver_id       VARCHAR(32) NOT NULL,
+            lat             FLOAT,
+            lng             FLOAT,
+            sent_f          VARCHAR(20)
+        );
+
+        /* Copy command */
+        {0};
+
+        ALTER TABLE tmp_availabilities DROP COLUMN temp;
+        ALTER TABLE tmp_availabilities ALTER COLUMN sent_f TYPE TIMESTAMP USING TO_TIMESTAMP(sent_f, 'YY-MM-DD HH24:MI:SS');
+
+        /* first time select into availabilities, second time insert into availabilities*/
+        {1};
+
+        DROP TABLE tmp_availabilities;
+
+        SELECT * from availabilities limit 5;
+    """
+
+    copy_cmd = """COPY tmp_availabilities FROM '{0}' WITH (format csv, header true, delimiter ',');""".format(files[fn])
+    # if exists compressed version of file    
+    if sys.platform == 'win32' and os.path.exists(files[fn] + '.gz'):
+        # path to 7zip
+        seven_zip = which('7z')
+        if seven_zip == None:
+            print('WARNING: can not locate program 7z to copy compressed file into DB')
+            print('- In Windows, Postgre can not handle big plain CSV files!')
+            print('- It is probabily that this uploading ends in an exception.')
+        else:
+            copy_cmd = """COPY tmp_availabilities FROM PROGRAM '"{0}" e -so "{1}"' DELIMITER ',' CSV HEADER;""".format(seven_zip, files[fn] + '.gz')
+
+    df = utils.run_query(create_table_from_csv.format(copy_cmd, _tpl.format(agency)), df=False)
+
+    df = utils.run_query("""select * from availabilities limit 5;""")
+    if len(df) == 5:
+        print('File {} loaded!'.format(fn))
+
+print('Counting rows in availabilities...')
+df = utils.run_query("""SELECT COUNT(1) FROM availabilities""")
+print('Total rows found: {}'.format(df.iloc[0,0]))
+
+print('Creating simple indexes for table availabilities (slow process)...')
+
+create_simple_index_tpl = 'CREATE INDEX {0}_aidx ON availabilities USING btree ({0});'
+
+for idx in ['id', 'distribution_center', 'driver_id', 'sent_f']:
+    print('Creating index "{}_aidx"...'.format(idx))
+    r = utils.run_query(create_simple_index_tpl.format(idx), df=False)
+
+print('[TODO]: we need to evaluate the creation of better indexes based in our requirements.')
+
+df = utils.run_query("""SELECT pg_size_pretty(pg_database_size(current_database())) as size""")
+print('Current DB Size: {}'.format(df.iloc[0,0]))
+
+print('PROCESS FINISHED!')
