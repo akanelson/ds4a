a8072813a876018f0fff80d2c981a0d7ee61e44f
kecol <ezequiel.bidart@gmail.com>
2020-05-23 19:46:57 -0300

    added realtime map!

--- ds4a/components/realtime.py
+++ ds4a/components/realtime.py
@@ -1,5 +1,16 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objects as go
+from datetime import datetime
+from datetime import timedelta
+from practicum_utils import get_loggi_files, global_connect, run_query, explained_time, careful_query
+import numpy as np
+
+# ----------------------
+# hard coded agencies ID
+# ----------------------
+agency = {'1': '6e7dacf2149d053183fe901e3cfd8b82', '2': '58cfe3b975dd7cbd1ac84d555640bfd9'}
+
 
 def realtime_map(current_date=None, current_time=None, current_agency=None):
     markup = dcc.Loading(
@@ -7,10 +18,69 @@ def realtime_map(current_date=None, current_time=None, current_agency=None):
         html.Div(
                 [
                     html.Div('Snapshot geographical distribution', className='analytics-title'),
-                    'Hola mapa'
+                    get_map(current_date, current_time, current_agency)
                 ],
                 id='realtime-map',
                 className="map-wrapper",
         )
     ], id='loading-map', className='analytics-loading')
     return markup
+
+
+def get_map(current_date, current_time, current_agency):
+
+    lat, lng = np.array([0]), np.array([0])
+
+    if current_date != None:
+        today = current_date[:10] + ' ' + current_time
+        now = datetime.strptime(today, '%Y-%m-%d %H:%M:%S')
+        from_ = now - timedelta(minutes=int(5))
+        #print(now, from_)
+
+        df = careful_query("""
+            select driver_id, avg(lat) as lat, avg(lng) as lng
+            from availability a 
+            where sent_f =  '{1}'
+            --and sent_f <= '{1}'
+            and distribution_center = '{0}'
+            group by driver_id
+        """.format(agency[current_agency], str(now), str(from_)))
+
+        print('available rows:', len(df))
+
+        print(df.shape)
+        #df = df.groupby('driver_id')[['lat', 'lng']].mean()
+        df = df[['lat', 'lng']]
+        print(df.shape)
+        lat = df['lat']
+        lng = df['lng']
+
+
+    if 1==1:
+        fig = go.Figure()
+        fig.add_trace(go.Densitymapbox(lat=lat, lon=lng, radius=10))
+    else:
+        fig = go.Figure(go.Scattermapbox(
+                lat=lat,
+                lon=lng,
+                mode='markers',
+                marker=go.scattermapbox.Marker(
+                    size=9
+                ),
+            ))
+
+    fig.update_geos(fitbounds="locations")
+    
+    fig.update_layout(
+        margin ={'l':0,'t':0,'b':0,'r':0},
+        mapbox = {
+        #'accesstoken': 'TOKEN',
+        'center': {'lat': lat.mean(), 'lon': lng.mean()},
+        'style': 'carto-positron', #'open-street-map',
+        'zoom': 11})
+
+
+
+    return html.Div([
+        dcc.Graph(figure=fig)
+    ])
